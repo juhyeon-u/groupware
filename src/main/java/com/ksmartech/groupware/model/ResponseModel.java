26557150cf5cdbed5f3a637d968953733afd0bd3
DESKTOP-HKM7BK8\kst <yujuhyeon@ksmartech.com>
2021-04-05 18:32:07 +0900

    Add ResponseModel for API

---
+++ src/main/java/com/ksmartech/groupware/model/ResponseModel.java
@@ -0,0 +1,42 @@
+package com.ksmartech.groupware.model;
+
+
+public class ResponseModel {
+
+    private String code;
+    private String message;
+    private Object data;
+
+    @Override
+    public String toString() {
+        return "ResponseModel{" +
+                "code='" + code + '\'' +
+                ", message='" + message + '\'' +
+                ", data=" + data +
+                '}';
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
